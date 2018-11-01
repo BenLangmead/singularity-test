e951094ac9e8c00d54cff28defcc303a29477e69
Ben Langmead <ben.langmead@gmail.com>
2018-11-01 09:07:39 -0400

    after_commit_1.sh

--- after_commit_1.sh
+++ after_commit_1.sh
@@ -2,18 +2,38 @@
 
 # 2482a2cb640df4cf45c50ffcee30b29ae8c0ce1b
 
-set -x
+set -ex
 
-singularity help shub://BenLangmead/singularity-test
+get_help() {
+    url=$1
+    img=$2
+    singularity pull ${url} 2>/dev/null 1>&2
+    singularity help ${img} | grep '.'
+}
 
-singularity help shub://BenLangmead/singularity-test:master
+get_help shub://BenLangmead/singularity-test \
+	 ${SINGULARITY_CACHEDIR}/BenLangmead-singularity-test-master-latest.simg
+rm -f ${SINGULARITY_CACHEDIR}/BenLangmead-singularity-test*
 
-singularity help shub://BenLangmead/singularity-test@master
+# This doesn't work:
+# get_help shub://BenLangmead/singularity-test:master
+# Gives: ERROR Cannot find image. Is your capitalization correct?
 
-singularity help shub://BenLangmead/singularity-test:suffix
+get_help shub://BenLangmead/singularity-test@master \
+	 ${SINGULARITY_CACHEDIR}/BenLangmead-singularity-test-master-latest.simg
+rm -f ${SINGULARITY_CACHEDIR}/BenLangmead-singularity-test*
 
-singularity help shub://BenLangmead/singularity-test@suffix
+get_help shub://BenLangmead/singularity-test:suffix \
+	 ${SINGULARITY_CACHEDIR}/BenLangmead-singularity-test-master-suffix.simg
+rm -f ${SINGULARITY_CACHEDIR}/BenLangmead-singularity-test*
 
-singularity help shub://BenLangmead/singularity-test@2482a2cb640df4cf45c50ffcee30b29ae8c0ce1b
+# OK, this one is odd.  Seems to just completely ignore my specified suffix.
+get_help shub://BenLangmead/singularity-test@suffix \
+	 ${SINGULARITY_CACHEDIR}/BenLangmead-singularity-test-master-latest.simg
+rm -f ${SINGULARITY_CACHEDIR}/BenLangmead-singularity-test*
+
+get_help shub://BenLangmead/singularity-test@2482a2cb640df4cf45c50ffcee30b29ae8c0ce1b \
+	 ${SINGULARITY_CACHEDIR}/BenLangmead-singularity-test-master-latest.simg
+rm -f ${SINGULARITY_CACHEDIR}/BenLangmead-singularity-test*
 
 
