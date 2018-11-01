2c07f2c1b98881dad40442ffdbe79a51288c2996
Ben Langmead <langmea@cs.jhu.edu>
2018-11-01 08:51:26 -0400

    add after-commit-1 scrip

---
+++ after_commit_1.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# 2482a2cb640df4cf45c50ffcee30b29ae8c0ce1b
+
+set -x
+
+singularity help shub://BenLangmead/singularity-test
+
+singularity help shub://BenLangmead/singularity-test:master
+
+singularity help shub://BenLangmead/singularity-test@master
+
+singularity help shub://BenLangmead/singularity-test:suffix
+
+singularity help shub://BenLangmead/singularity-test@suffix
+
+singularity help shub://BenLangmead/singularity-test@2482a2cb640df4cf45c50ffcee30b29ae8c0ce1b
+
+
